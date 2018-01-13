f2bea5872061c62592c0e4400559d78ecf3635eb
hada gunjyo <35358161+gunjyo@users.noreply.github.com>
2018-01-14 01:45:05 +0900

    fn.g fn.q fn.rnum fn.rword

--- use.js
+++ use.js
@@ -1,3 +1,21 @@
 var fn=this.fn||{};
  fn.g=(s)=>{return document.getElementById(s)};
  fn.q=(s)=>{return document.querySelector(s)};
+ fn.rnum=(l=8)=>{
+  var c = "123456789";//0を含めない方が都合が良い
+  var cl=c.length;
+  var r = "";
+  for(var i=0; i<l; i++){
+      r += c[Math.floor(Math.random()*cl)];
+  } 
+   return r;
+ };
+ fn.rword=(l=8)=>{
+  var c = "abcdefghijklmnopqrstuvwxyz0123456789";
+  var cl=c.length;
+  var r = "";
+  for(var i=0; i<l; i++){
+      r += c[Math.floor(Math.random()*cl)];
+  } 
+   return r;
+ };
